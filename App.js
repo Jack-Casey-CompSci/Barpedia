5ec5c035347d167abeca7d60c89ae72b818f15b1
Dylan <dylancool16@gmail.com>
2020-07-21 20:40:04 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,30 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Dimensions, Text, View, Button, Image, FlatList, ImageBackgroundComponent } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Dimensions,
+  Text,
+  View,
+  Button,
+  Image,
+  FlatList,
+  ImageBackgroundComponent,
+} from "react-native";
 import barsList from "./data/bars.json";
-import BarCard from "./components/BarCard.js"
+import BarCard from "./components/BarCard.js";
+import BarPage from "./components/BarPage.js";
 
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 export default function App() {
   return (
-    <FlatList style={styles.container}
+    <FlatList
+      style={styles.container}
       data={barsList}
-      renderItem= {({item}) => {
-        return(
-          <BarCard key={item.id} barName={item.name} barDescription={item.description}/>
-          )
+      renderItem={({ item }) => {
+        return (
+          <BarCard
+            key={item.id}
+            barName={item.name}
+            barDescription={item.description}
+          />
+        );
       }}
     />
-    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 30
-  }
+    marginTop: 30,
+  },
 });
-

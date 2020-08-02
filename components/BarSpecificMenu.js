68be4e9cd77b8c97a3994cafd7ce319e6fe20166
Dylan <dylancool16@gmail.com>
2020-08-02 19:45:50 -0400

    Apply prettier to new files

--- components/BarSpecificMenu.js
+++ components/BarSpecificMenu.js
@@ -1,13 +1,21 @@
 import React from "react";
-import { ScrollView, Image, StyleSheet, View, Dimensions, Modal } from "react-native"
+import {
+  ScrollView,
+  Image,
+  StyleSheet,
+  View,
+  Dimensions,
+  Modal,
+} from "react-native";
 import ImageViewer from "react-native-image-zoom-viewer";
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
-const images = [{
+const images = [
+  {
     // Simplest usage.
-    url: '',
+    url: "",
 
     // width: number
     // height: number
@@ -15,23 +23,23 @@ const images = [{
 
     // You can pass props to <Image />.
     props: {
-        source: require("../assets/barBleuMenu.png")
-        // headers: ...
-    }
-}
-]
+      source: require("../assets/barBleuMenu.png"),
+      // headers: ...
+    },
+  },
+];
 
 export default function BarSpecificMenu() {
-    return (
-        <Modal visible={true} trasparent={true}>
-            <ImageViewer imageUrls={images} />
-        </Modal>
-    )
+  return (
+    <Modal visible={true} trasparent={true}>
+      <ImageViewer imageUrls={images} />
+    </Modal>
+  );
 }
 
 const styles = StyleSheet.create({
-    tinyLogo: {
-        height: "100%",
-        width: "100%"
-    }
-})
+  tinyLogo: {
+    height: "100%",
+    width: "100%",
+  },
+});
--- components/EventsSpecials.js
+++ components/EventsSpecials.js
@@ -1,60 +1,71 @@
-import React from 'react';
-import { StyleSheet, Text, SafeAreaView, ScrollView, View, FlatList } from 'react-native';
-import Constants from 'expo-constants';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  View,
+  FlatList,
+} from "react-native";
+import Constants from "expo-constants";
 
-import DayBox from "./dayBox.js"
+import DayBox from "./dayBox.js";
 import barsList from "../data/specials.json";
 
 export default function EventsSpecials({ name }) {
-    const barData = barsList.find((element) => {
-        return element.name === name;
-    })
-    return (
-        <View style={styles.box}>
-            <FlatList
-                style={styles.container}
-                data={barData.days}
-                horizontal={true}
-                renderItem={({ item }) => {
-                    return (
-                        <>
-                            <DayBox
-                                key={item.id}
-                                day={item.day}
-                                drinkSpecial1Description={item.info.DrinkSpecials.Event1.Description}
-                                drinkSpecial1Time={item.info.DrinkSpecials.Event1.Time}
-                                drinkSpecial2Description={item.info.DrinkSpecials.Event2.Description}
-                                drinkSpecial2Time={item.info.DrinkSpecials.Event2.Time}
-                                foodSpecialDescription={item.info.FoodSpecials.Description}
-                                foodSpecialTime={item.info.FoodSpecials.Time}
-                                eventDescription={item.info.Events.Description}
-                                eventTime={item.info.Events.Time}
-                            ></DayBox>
-                        </>
-                    );
-                }}
-            />
-        </View>
-    );
+  const barData = barsList.find((element) => {
+    return element.name === name;
+  });
+  return (
+    <View style={styles.box}>
+      <FlatList
+        style={styles.container}
+        data={barData.days}
+        horizontal={true}
+        renderItem={({ item }) => {
+          return (
+            <>
+              <DayBox
+                key={item.id}
+                day={item.day}
+                drinkSpecial1Description={
+                  item.info.DrinkSpecials.Event1.Description
+                }
+                drinkSpecial1Time={item.info.DrinkSpecials.Event1.Time}
+                drinkSpecial2Description={
+                  item.info.DrinkSpecials.Event2.Description
+                }
+                drinkSpecial2Time={item.info.DrinkSpecials.Event2.Time}
+                foodSpecialDescription={item.info.FoodSpecials.Description}
+                foodSpecialTime={item.info.FoodSpecials.Time}
+                eventDescription={item.info.Events.Description}
+                eventTime={item.info.Events.Time}
+              ></DayBox>
+            </>
+          );
+        }}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    box: {
-        flexDirection: "row"
-    },
-    container: {
-        marginTop: Constants.statusBarHeight,
-        height: 250
-    },
-    title: {
-        flex: 3,
-        borderWidth: 1,
-        borderColor: "black",
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    titleText: {
-        fontWeight: "bold",
-        fontSize: 14
-    }
+  box: {
+    flexDirection: "row",
+  },
+  container: {
+    marginTop: Constants.statusBarHeight,
+    height: 250,
+  },
+  title: {
+    flex: 3,
+    borderWidth: 1,
+    borderColor: "black",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  titleText: {
+    fontWeight: "bold",
+    fontSize: 14,
+  },
 });

2784eef3a1db07a51ba4b51e40f59c7596aa91f0
Dylan <dylancool16@gmail.com>
2020-08-24 17:17:38 -0400

    Update everyday accordion with map function

--- components/AccordionFiles/everydayAccordion.js
+++ components/AccordionFiles/everydayAccordion.js
@@ -12,91 +12,13 @@ export default function everydayAccordion({ name }) {
       console.log(data, key);
       return (
         <View style={styles.container}>
-          <View style={styles.row}>
-            <View style={styles.box}>
-              <Text style={styles.description}>{data.Drink}</Text>
-              <Text style={styles.price}>{data.Price}</Text>
-            </View>
+          <View style={styles.box}>
+            <Text style={styles.description}>{data.Drink}</Text>
+            <Text style={styles.price}>{data.Price}</Text>
           </View>
         </View>
       );
     });
-    /*
-      return (
-        <View style={styles.container}>
-          <View style={styles.row}>
-            <View style={styles.box}>
-              <Text style={styles.description}>
-                {barData.everyday.Deal1.Drink}
-              </Text>
-              <Text style={styles.price}>{barData.everyday.Deal1.Price}</Text>
-            </View>
-          </View>
-        </View>
-      );
-*/
-    /*
-      return (
-        <View style={styles.container}>
-          <View style={styles.row}>
-            <View style={styles.box}>
-              <Text style={styles.description}>
-                {barData.everyday.Deal1.Drink}
-              </Text>
-              <Text style={styles.price}>{barData.everyday.Deal1.Price}</Text>
-            </View>
-            <View style={styles.box}>
-              <Text style={styles.description}>
-                {barData.everyday.Deal2.Drink}
-              </Text>
-              <Text style={styles.price}>{barData.everyday.Deal2.Price}</Text>
-            </View>
-          </View>
-          <View style={styles.row}>
-            <View style={styles.box}>
-              <Text style={styles.description}>
-                {barData.everyday.Deal3.Drink}
-              </Text>
-              <Text style={styles.price}>{barData.everyday.Deal3.Price}</Text>
-            </View>
-            <View style={styles.box}>
-              <Text style={styles.description}>
-                {barData.everyday.Deal4.Drink}
-              </Text>
-              <Text style={styles.price}>{barData.everyday.Deal4.Price}</Text>
-            </View>
-          </View>
-          <View style={styles.row}>
-            <View style={styles.box}>
-              <Text style={styles.description}>
-                {barData.everyday.Deal5.Drink}
-              </Text>
-              <Text style={styles.price}>{barData.everyday.Deal5.Price}</Text>
-            </View>
-            <View style={styles.box}>
-              <Text style={styles.description}>
-                {barData.everyday.Deal6.Drink}
-              </Text>
-              <Text style={styles.price}>{barData.everyday.Deal6.Price}</Text>
-            </View>
-          </View>
-          <View style={styles.row}>
-            <View style={styles.box}>
-              <Text style={styles.description}>
-                {barData.everyday.Deal7.Drink}
-              </Text>
-              <Text style={styles.price}>{barData.everyday.Deal7.Price}</Text>
-            </View>
-            <View style={styles.box}>
-              <Text style={styles.description}>
-                {barData.everyday.Deal8.Drink}
-              </Text>
-              <Text style={styles.price}>{barData.everyday.Deal8.Price}</Text>
-            </View>
-          </View>
-        </View>
-      );
-      */
   } else {
     return <Text>This is not offered at this bar</Text>;
   }

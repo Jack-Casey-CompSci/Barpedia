23836a61e6076efad9a22f82b95ce67d10d6d363
alexrang83 <asr5405@psu.edu>
2020-08-24 14:29:51 -0400

    Added happy hour accordion section

--- components/AccordionFiles/happyHourAccordion.js
+++ components/AccordionFiles/happyHourAccordion.js
@@ -1,55 +1,75 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
+import EverydayValues from "../../data/happyHour.json";
+
 export default function HappyHourAccordion({ name }) {
   const barData = EverydayValues.find((element) => {
     return element.name === name;
   });
-  return (
-    <View style={styles.container}>
-      <View style={styles.row}>
-        <View style={styles.box}>
-          <Text style={styles.description}>Vodka Redbull</Text>
-          <Text style={styles.price}>$4.25</Text>
+
+  console.log(barData)
+  if (barData.available) {
+    return barData.happyHour.map((data, key) => {
+      console.log(data, key);
+      return (
+        <View style={styles.container}>
+          <View style={styles.row}>
+            <View style={styles.box}>
+              <Text style={styles.description}>{data.description}</Text>
+              <Text style={styles.price}>{data.price}</Text>
+            </View>
+          </View>
         </View>
-        <View style={styles.box}>
-          <Text style={styles.description}>Purple Gatorade</Text>
-          <Text style={styles.price}>$3.00</Text>
-        </View>
-      </View>
-      <View style={styles.row}>
-        <View style={styles.box}>
-          <Text style={styles.description}>Green Tea</Text>
-          <Text style={styles.price}>$2.50</Text>
-        </View>
-        <View style={styles.box}>
-          <Text style={styles.description}>Irish Car Bomb</Text>
-          <Text style={styles.price}>$2.75</Text>
-        </View>
-      </View>
-      <View style={styles.row}>
-        <View style={styles.box}>
-          <Text style={styles.description}>Vodka Redbull</Text>
-          <Text style={styles.price}>$4.25</Text>
-        </View>
-        <View style={styles.box}>
-          <Text style={styles.description}>Purple Gatorade</Text>
-          <Text style={styles.price}>$3.00</Text>
-        </View>
-      </View>
-      <View style={styles.row}>
-        <View style={styles.box}>
-          <Text style={styles.description}>Green Tea</Text>
-          <Text style={styles.price}>$2.50</Text>
-        </View>
-        <View style={styles.box}>
-          <Text style={styles.description}>Irish Car Bomb</Text>
-          <Text style={styles.price}>$2.75</Text>
-        </View>
-      </View>
-    </View>
-  );
+      );
+    });
+  }
 }
+// return (
+//   <View style={styles.container}>
+//     <View style={styles.row}>
+//       <View style={styles.box}>
+//         <Text style={styles.description}>Vodka Redbull</Text>
+//         <Text style={styles.price}>$4.25</Text>
+//       </View>
+//       <View style={styles.box}>
+//         <Text style={styles.description}>Purple Gatorade</Text>
+//         <Text style={styles.price}>$3.00</Text>
+//       </View>
+//     </View>
+//     <View style={styles.row}>
+//       <View style={styles.box}>
+//         <Text style={styles.description}>Green Tea</Text>
+//         <Text style={styles.price}>$2.50</Text>
+//       </View>
+//       <View style={styles.box}>
+//         <Text style={styles.description}>Irish Car Bomb</Text>
+//         <Text style={styles.price}>$2.75</Text>
+//       </View>
+//     </View>
+//     <View style={styles.row}>
+//       <View style={styles.box}>
+//         <Text style={styles.description}>Vodka Redbull</Text>
+//         <Text style={styles.price}>$4.25</Text>
+//       </View>
+//       <View style={styles.box}>
+//         <Text style={styles.description}>Purple Gatorade</Text>
+//         <Text style={styles.price}>$3.00</Text>
+//       </View>
+//     </View>
+//     <View style={styles.row}>
+//       <View style={styles.box}>
+//         <Text style={styles.description}>Green Tea</Text>
+//         <Text style={styles.price}>$2.50</Text>
+//       </View>
+//       <View style={styles.box}>
+//         <Text style={styles.description}>Irish Car Bomb</Text>
+//         <Text style={styles.price}>$2.75</Text>
+//       </View>
+//     </View>
+//   </View>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {

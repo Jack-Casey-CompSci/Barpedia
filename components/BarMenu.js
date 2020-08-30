659bb845ea3fe7cc4f261e5f2b6ea0c8af6fa777
alexrang83 <asr5405@psu.edu>
2020-08-30 15:19:11 -0400

    Added bar menu refresh

--- components/BarMenu.js
+++ components/BarMenu.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, FlatList, Image, Dimensions } from "react-native";
+import { StyleSheet, FlatList, Image, Dimensions, RefreshControl } from "react-native";
 import BarCard from "./BarCard.js";
 import barsList from "../data/bars.json";
 import logo from "../assets/Barpedia_logo.png";
@@ -8,8 +8,24 @@ import { ScrollView } from "react-native-gesture-handler";
 const windowWidth = Dimensions.get("window").width;
 
 export default function BarMenu({ navigation }) {
+  const [refreshing, setRefreshing] = React.useState(false);
+  const wait = (timeout) => {
+    return new Promise(resolve => {
+      setTimeout(resolve, timeout);
+    });
+  }
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+
+    wait(1000).then(() => setRefreshing(false));
+  }, []);
   return (
-    <ScrollView style={styles.container}>
+
+    <ScrollView
+      style={styles.container}
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }>
       <FlatList
         style={styles.container}
         data={barsList}

83a5bd5b78706d75e9d6cf99ee1d85adb59d3d1b
alexrang83 <asr5405@psu.edu>
2021-01-31 11:15:26 -0500

    Working version

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import BarPage from "./components/BarPage.js";
 import BarFood from "./components/PictureLinkers/BarSpecificMenu.js";
 import BarDrinks from "./components/PictureLinkers/BarSpecificDrinks.js";
 import LineReporting from "./components/ReportForm.js";
+import Map from "./components/Map.js"
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -17,7 +18,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Navigator>
-        <Screen name="Home" component={BarMenu} />
+        <Screen name="Home" component={Map} />
         <Screen name="Details" component={BarPage} />
         <Screen name="Bar Food" component={BarFood} />
         <Screen name="Bar Drinks" component={BarDrinks} />

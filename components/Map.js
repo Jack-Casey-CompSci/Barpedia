f085ca3b811745403901aee3f653ef6bf2ac02f4
alexrang83 <asr5405@psu.edu>
2021-02-01 14:13:04 -0500

    Added a few markers to map

--- components/Map.js
+++ components/Map.js
@@ -1,18 +1,32 @@
 import * as React from 'react';
-import MapView from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import StoreMarkers from "../data/stores.json";
+import BeerPic from "../assets/beer.png";
 
-export default function Map() {
+export default class Map extends React.Component {
+  render(){
   return (
     <View style={styles.container}>
       <MapView style={styles.map} initialRegion={{
-      latitude: 37.78825,
-      longitude: -122.4324,
+      latitude: 40.79331567231344, 
+      longitude: -77.86254004954345,
       latitudeDelta: 0.0922,
       longitudeDelta: 0.0421,
-    }} />
+    }}>
+    {StoreMarkers.map((marker, index) => (
+    <Marker
+      key={index}
+      coordinate={marker.latlng}
+      title={marker.name}
+      description={marker.description}
+      image={BeerPic}
+    />
+    ))}
+    </MapView>
     </View>
   );
+}
 }
 
 const styles = StyleSheet.create({
